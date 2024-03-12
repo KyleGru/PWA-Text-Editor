d2360f4e1857eb8206c08cde6088fc6c46a3d44d
KyleGru <136273960+KyleGru@users.noreply.github.com>
2024-03-11 20:19:28 -0400

    Added logic for installing PWA in install.js and logic for adding content to the DB and getting content from the DB

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,54 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+      hot: 'only'
+    },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Client Server'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'dist-sw.js'
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text-Editor',
+        description: 'Edit text online or offline',
+        start_url: './',
+        publicPath: './',
+        icons: [{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }],
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime']
+            }
+          } 
+        },
       ],
     },
   };
